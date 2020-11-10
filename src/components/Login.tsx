a1690f3592ccfedee01e17c44a8c4394f1ff8888
sanhernandezmon <Shm1qazz>
2020-11-09 21:30:31 -0500

    login chaanges

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -8,12 +8,16 @@ import {
     IonTitle,
     IonToolbar
 } from '@ionic/react';
-import React from 'react';
+import React, {useRef} from 'react';
 import {personCircle} from "ionicons/icons";
+import firebase from "firebase";
+import firebaseApp from "../firebase/firebaseInit";
+import {log} from "util";
 
 const Login: React.FC = () => {
 
-
+    const myemail = useRef<HTMLIonInputElement>(null);
+    const mypass = useRef<HTMLIonInputElement>(null);
     return (
         <IonPage>
             <IonHeader>
@@ -39,6 +43,7 @@ const Login: React.FC = () => {
                             <IonLabel position="floating"> Email</IonLabel>
                             <IonInput
                                 type="email"
+                                ref={myemail}
                             >
                             </IonInput>
                         </IonItem>
@@ -46,6 +51,7 @@ const Login: React.FC = () => {
                             <IonLabel position="floating"> Password</IonLabel>
                             <IonInput
                                 type="password"
+                                ref={mypass}
                             >
                             </IonInput>
                         </IonItem>
@@ -58,7 +64,26 @@ const Login: React.FC = () => {
                         <p style={{ fontSize: "small" }}>
                             By clicking LOGIN you agree to our <a href="#">Policy</a>
                         </p>
-                        <IonButton expand="block" >
+                        <IonButton expand="block" onClick={
+                            ()=>{
+                                firebaseApp.auth().signInWithEmailAndPassword(myemail.current?.value as string,
+                                    mypass.current?.value as string).then(user=>{
+                                    console.log(user)
+                                    firebaseApp.auth().currentUser?.getIdTokenResult()
+                                        .then((idTokenResult) => {
+                                            // Confirm the user is an Admin.
+                                            console.log(idTokenResult.claims)
+                                        })
+                                        .catch((error) => {
+                                            console.log(error);
+                                        });
+                                }).catch((error)=>{
+                                    console.log(error)
+                                })
+
+
+                            }
+                        }>
                         Login
                     </IonButton>
                     </IonCol>
